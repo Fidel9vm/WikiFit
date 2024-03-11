a7f60f0aab2b49dbedf9930edfafd64b0b8cf80f
Fidel9vm <fideljlopez9@gmail.com>
2024-03-10 21:22:11 -0300

    Base de la página completa (sin infos y js)

---
+++ codigo.js
@@ -0,0 +1,9 @@
+function showhide(){
+    var backexs = document.getElementById('backexs');
+
+    if(backexs.style.display == "none"){
+        backexs.style.display = "block";
+    }else{
+        backexs.style.display = "none";
+    }
+}
